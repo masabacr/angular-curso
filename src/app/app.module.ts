e5ebc82d931de846602894e164bf25cb03fe0aeb
msaenz <msaenz@ccss.sa.cr>
2021-07-14 14:37:22 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   // ContadorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
